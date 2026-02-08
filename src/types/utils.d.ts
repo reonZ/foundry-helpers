0f2bc1f2e06e0ceddbf3505a39de2afdf6cb3d24
Idle <651462+reonZ@users.noreply.github.com>
2026-02-08 20:06:41 +0100

    remove duplicate types

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -4,8 +4,6 @@ declare global {
     type EventType = keyof HTMLElementEventMap;
     type MaybeHTML = Maybe<Document | Element | EventTarget>;
 
-    type WithPartial<T extends Record<string, any>, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-    type WithRequired<T extends Record<string, any>, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
     type PartialExcept<T extends Record<string, any>, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
     type RequiredExcept<T extends Record<string, any>, K extends keyof T> = Required<Omit<T, K>> & Pick<T, K>;
 }
