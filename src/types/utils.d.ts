13d0b469c682e853035af212035dfb0b09cb233e
Idle <651462+reonZ@users.noreply.github.com>
2026-02-09 12:29:40 +0100

    add RequireAtLeastOne & RequireOnlyOne types

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -6,6 +6,10 @@ declare global {
 
     type PartialExcept<T extends Record<string, any>, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
     type RequiredExcept<T extends Record<string, any>, K extends keyof T> = Required<Omit<T, K>> & Pick<T, K>;
+    type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+        { [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>> }[Keys];
+    type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+        { [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>> }[Keys];
 
     type Rectangle = {
         x: number;
