bd8d2043e95d7ee50f23bc59a191fb3d006a0e9b
Idle <651462+reonZ@users.noreply.github.com>
2026-03-08 13:54:04 +0100

    make COIN_COMPENDIUM_UUIDS system agnostic

--- src/pf2e/inventory.ts
+++ src/pf2e/inventory.ts
@@ -1,13 +1,13 @@
 import { ItemPF2e, PhysicalItemType } from "@7h3laughingman/pf2e-types";
-import { R } from "..";
+import { R, SYSTEM } from "..";
 
 const COIN_DENOMINATIONS = ["pp", "gp", "sp", "cp"] as const;
 
 const COIN_COMPENDIUM_UUIDS = {
-    pp: "Compendium.pf2e.equipment-srd.Item.JuNPeK5Qm1w6wpb4",
-    gp: "Compendium.pf2e.equipment-srd.Item.B6B7tBWJSqOBz5zz",
-    sp: "Compendium.pf2e.equipment-srd.Item.5Ew82vBF9YfaiY9f",
-    cp: "Compendium.pf2e.equipment-srd.Item.lzJ8AVhRcbFul5fh",
+    pp: SYSTEM.uuid("Compendium.pf2e.equipment-srd.Item.JuNPeK5Qm1w6wpb4"),
+    gp: SYSTEM.uuid("Compendium.pf2e.equipment-srd.Item.B6B7tBWJSqOBz5zz"),
+    sp: SYSTEM.uuid("Compendium.pf2e.equipment-srd.Item.5Ew82vBF9YfaiY9f"),
+    cp: SYSTEM.uuid("Compendium.pf2e.equipment-srd.Item.lzJ8AVhRcbFul5fh"),
 };
 
 let _physicalItemTypes: PhysicalItemType[] | undefined;
